df3df2bb58d99180ef68432ba0cfad3da4ed5d90
Adil Zholdaspaev <adilzholdaspaev@gmail.com>
2018-03-20 21:42:46 +0300

    Refactoring

--- flash-pipelines-parent/pipelines-core/src/main/java/com/flashpipelines/core/boot/SpringExtension.java
+++ flash-pipelines-parent/pipelines-core/src/main/java/com/flashpipelines/core/boot/SpringExtension.java
@@ -5,10 +5,13 @@ import akka.actor.Extension;
 import akka.actor.Props;
 import com.flashpipelines.core.Service;
 import com.flashpipelines.core.actor.ActorReference;
-import com.flashpipelines.core.actor.ActorType;
 import org.springframework.context.ApplicationContext;
 
 import java.util.List;
+
+import static com.flashpipelines.core.actor.ActorType.FINALIZER_ACTOR;
+import static com.flashpipelines.core.actor.ActorType.SIMPLE_ACTOR;
+import static com.flashpipelines.core.actor.ActorType.SUPERVISER_ACTOR;
 
 public class SpringExtension implements Extension {
 
@@ -19,14 +22,14 @@ public class SpringExtension implements Extension {
     }
 
     public Props props(Service service) {
-        return Props.create(SpringActorProducer.class, applicationContext, ActorType.FINALIZER_ACTOR, service);
+        return Props.create(SpringActorProducer.class, applicationContext, FINALIZER_ACTOR, new Object[] {service});
     }
 
     public Props props(Service service, ActorRef sendTo) {
-        return Props.create(SpringActorProducer.class, applicationContext, ActorType.SIMPLE_ACTOR, service, sendTo);
+        return Props.create(SpringActorProducer.class, applicationContext, SIMPLE_ACTOR, new Object[] {service, sendTo});
     }
 
     public Props props(List<ActorReference> actorReferences) {
-        return Props.create(SpringActorProducer.class, applicationContext, ActorType.SUPERVISER_ACTOR, actorReferences, this);
+        return Props.create(SpringActorProducer.class, applicationContext, SUPERVISER_ACTOR, new Object[] {actorReferences, this});
     }
 }
